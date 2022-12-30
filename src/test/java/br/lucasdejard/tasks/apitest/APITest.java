70eb83d6c339d9574a3daa95ba5871e8ce003fed
Lucas Dejard <34687381+lucasdejard@users.noreply.github.com>
2022-12-30 01:39:43 -0300

    first commit

---
+++ src/test/java/br/lucasdejard/tasks/apitest/APITest.java
@@ -0,0 +1,38 @@
+package br.lucasdejard.tasks.apitest;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+
+public class APITest {
+	
+	@BeforeClass
+	public static void setup( ) {
+		RestAssured.baseURI = "http://localhost:8001/tasks-backend";
+	}
+	
+	@Test
+	public void	deveRetornarTarefas(){
+		RestAssured.given()
+			.log().all()
+		.when()
+		.get("/todo")
+		.then()
+			.statusCode(200)
+		;
+		}
+
+	@Test
+	public void	deveAdicionarTarefaComSucesso(){
+		RestAssured.given()
+			.body("{ \"task\": \"Teste via API\", \"dueDate\": \"2022-12-30\"}")
+			.contentType(ContentType.JSON)
+		.when()
+			.post("/todo")
+		.then()
+			.statusCode(201)
+		;
+		}
+}
